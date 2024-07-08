1fd7679268a68716c5609fe993a016a654775626
allenccbet <drakeccbet@gmail.com>
2024-07-08 11:27:25 -0700

    cdn

---
+++ seo/script.js
@@ -0,0 +1,17 @@
+function toggleNavbar(){
+    document.getElementById('navbar-default').classList.contains('hidden') ?
+    document.getElementById('navbar-default').classList.remove('hidden') :
+    document.getElementById('navbar-default').classList.add('hidden');
+}
+function register(){
+    window.location = 'https://cc-bet.com/#/register?invite_code=KM3LNB'
+}
+function downloadApp(){
+    window.location = 'https://cc-bet.app'
+}
+function gotoTelegram(){
+    window.location = 'https://t.me'
+}
+function customerService(){
+    window.location = 'https://t.me'
+}
